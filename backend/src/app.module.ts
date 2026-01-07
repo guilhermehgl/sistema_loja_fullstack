5cca777561e0d4486ab0a8b4ce509d25aa4aedcd
0-GBL <araujo.gabriel3289@gmail.com>
2026-01-07 20:08:13 -0300

    fix: correção de bug 1.0

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,11 +7,11 @@ import { OrdersModule } from './orders/orders.module'
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'estoque',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
       logging: true,
@@ -20,4 +20,4 @@ import { OrdersModule } from './orders/orders.module'
     OrdersModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
